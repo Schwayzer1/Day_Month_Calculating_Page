d2a4393d67c4e64134fd7a0fef28e5f71048dd6e
Schwayzer <furkann.onay@gmail.com>
2022-08-15 15:04:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
+
+
+var aylar = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım","Aralık"];
+
+
+let today = new Date();
+
+document.getElementById("date").innerHTML = `${today.getDate()}.${today.getMonth()+1}.${today.getFullYear()} - ${gunler[today.getDay()]}` ;
+
+
+
+
+function calculating() {
+
+    let gun = parseInt(document.getElementById("gun").value);
+
+    let ay = parseInt(document.getElementById("ay").value);
+
+    let sonuc1 = new Date(
+        new Date().getFullYear(),
+        new Date().getMonth(), 
+        new Date().getDate() + (gun ? gun:0)
+    ); 
+
+    document.getElementById("gunsonuc").innerHTML = `${sonuc1.getDate()}.${sonuc1.getMonth()+1}.${sonuc1.getFullYear()} ${gunler[sonuc1.getDay()]}` ;
+
+
+    let sonuc2 = new Date(
+        new Date().getFullYear(),
+        new Date().getMonth() + (ay ? ay:0), 
+        new Date().getDate()
+    ); 
+
+    document.getElementById("aysonuc").innerHTML = `${sonuc2.getDate()}.${sonuc2.getMonth()+1}.${sonuc2.getFullYear()} ${aylar[sonuc2.getMonth()]}` ;
+
+}
