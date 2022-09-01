693667bc53ca438189bdf59ddc7cca2eb279b609
Schwayzer <furkann.onay@gmail.com>
2022-09-01 15:22:14 +0300

    translated to english

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
-var gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
+var gunler = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-
-var aylar = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım","Aralık"];
-
+var aylar = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "Jully",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 let today = new Date();
 
-document.getElementById("date").innerHTML = `${today.getDate()}.${today.getMonth()+1}.${today.getFullYear()} - ${gunler[today.getDay()]}` ;
-
-
-
+document.getElementById("date").innerHTML = `${today.getDate()}.${
+  today.getMonth() + 1
+}.${today.getFullYear()} - ${gunler[today.getDay()]}`;
 
 function calculating() {
+  let gun = parseInt(document.getElementById("gun").value);
 
-    let gun = parseInt(document.getElementById("gun").value);
+  let ay = parseInt(document.getElementById("ay").value);
 
-    let ay = parseInt(document.getElementById("ay").value);
+  let sonuc1 = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    new Date().getDate() + (gun ? gun : 0)
+  );
 
-    let sonuc1 = new Date(
-        new Date().getFullYear(),
-        new Date().getMonth(), 
-        new Date().getDate() + (gun ? gun:0)
-    ); 
+  document.getElementById("gunsonuc").innerHTML = `${sonuc1.getDate()}.${
+    sonuc1.getMonth() + 1
+  }.${sonuc1.getFullYear()} ${gunler[sonuc1.getDay()]}`;
 
-    document.getElementById("gunsonuc").innerHTML = `${sonuc1.getDate()}.${sonuc1.getMonth()+1}.${sonuc1.getFullYear()} ${gunler[sonuc1.getDay()]}` ;
+  let sonuc2 = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + (ay ? ay : 0),
+    new Date().getDate()
+  );
 
-
-    let sonuc2 = new Date(
-        new Date().getFullYear(),
-        new Date().getMonth() + (ay ? ay:0), 
-        new Date().getDate()
-    ); 
-
-    document.getElementById("aysonuc").innerHTML = `${sonuc2.getDate()}.${sonuc2.getMonth()+1}.${sonuc2.getFullYear()} ${aylar[sonuc2.getMonth()]}` ;
-
+  document.getElementById("aysonuc").innerHTML = `${sonuc2.getDate()}.${
+    sonuc2.getMonth() + 1
+  }.${sonuc2.getFullYear()} ${aylar[sonuc2.getMonth()]}`;
 }
